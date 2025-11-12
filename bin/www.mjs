90ce7fa8382b11201f5b6f8832e281da02522f42
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-11-12 15:28:09 +0100

    Set open port automatically

--- bin/www.mjs
+++ bin/www.mjs
@@ -2,6 +2,8 @@ import { config } from "dotenv";
 import debug from "debug";
 import http from "http";
 
+import { initLivereload } from "../config/livereload.mjs";
+
 import app from "../app.mjs";
 
 config();
@@ -9,32 +11,12 @@ config();
 debug.enable(process.env.DEBUG);
 const log = debug("express-bootstrap:server");
 
+const { APP_PORT } = process.env;
+
 const DEFAULT_PORT = 3000;
+const DEFAULT_LIVERELOAD_PORT = 35729;
 
-/**
- * Get port from environment and store in Express.
- */
-
-let port = normalizePort(process.env.PORT || DEFAULT_PORT);
-app.set("port", port);
-
-/**
- * Create HTTP server.
- */
-
-let server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
-
-/**
- * Normalize a port into a number, string, or false. */
-
+// Normalize a port into a number, string, or false.
 function normalizePort(val) {
   let port = Number(val);
 
@@ -54,10 +36,56 @@ function normalizePort(val) {
 }
 
 /**
- * Event listener for HTTP server "error" event.
+ * Returns an open port
+ * @param {object} app - Express app
+ * @param {number} port - Env or default port
+ * @throws Whatever error the http server detects that is not an EADDRINUSE
+ * @returns {Promise<number>} port - Open port
  */
+async function getOpenPort(app, port) {
+  const mockServer = http.createServer(app);
 
-function onError(error) {
+  function checkPort() {
+    mockServer.listen(port);
+
+    return new Promise((resolve, reject) => {
+      mockServer.on("error", (error) => {
+        if (error.code === "EADDRINUSE") {
+          resolve(false);
+        } else {
+          reject(error);
+        }
+      });
+
+      mockServer.on("listening", () => {
+        mockServer.close(() => resolve(true));
+      });
+    });
+  }
+
+  while (!(await checkPort())) port++;
+  return port;
+}
+
+// Create HTTP server.
+let server = http.createServer(app);
+
+(async function init() {
+  let serverPort = normalizePort(APP_PORT);
+  serverPort = await getOpenPort(app, serverPort);
+
+  let livereloadPort = await getOpenPort(app, DEFAULT_LIVERELOAD_PORT);
+
+  app.set("port", serverPort);
+  initLivereload(app, livereloadPort);
+
+  server.listen(serverPort);
+  server.on("error", (error) => onError(error, serverPort));
+  server.on("listening", onListening);
+})();
+
+// Event listener for HTTP server "error" event.
+function onError(error, port) {
   if (error.syscall !== "listen") throw error;
 
   let bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
@@ -75,10 +103,7 @@ function onError(error) {
   process.exit(1);
 }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
-
+// Event listener for HTTP server "listening" event.
 function onListening() {
   let addr = server.address();
   let bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
